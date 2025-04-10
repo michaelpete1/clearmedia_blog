988b0cc510c3ef7280b5db7fd499494ab8fbf1be
Chidi Onyekwere <michaelpet40@gmail.com>
2025-04-10 11:23:12 +0100

    first commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,13 +8,13 @@ const isHovered = ref(false);
 
 <template>
   <nav
-    class="fixed top-0 left-0 w-full z-50 shadow-lg p-4 transition-opacity duration-300"
+    class="top-0 left-0 w-full z-50 shadow-lg p-4 transition-opacity duration-300"
     :class="isHovered ? 'bg-gray-900 opacity-100' : 'bg-gray-900/50 opacity-60'"
     @mouseenter="isHovered = true"
     @mouseleave="isHovered = false"
   >
     <div class="container mx-auto flex justify-between items-center">
-      <!-- ClearMedia Logo with Purple Glow on Hover -->
+      <!-- ClearMedia Logo -->
       <RouterLink
         to="/"
         class="text-2xl font-bold text-white transition duration-300 ease-in-out hover:text-purple-400 hover:drop-shadow-[0_0_8px_rgba(192,132,252,0.8)]"
@@ -22,14 +22,14 @@ const isHovered = ref(false);
         ClearMedia
       </RouterLink>
 
-      <!-- Hamburger Icon (visible on small screens) -->
+      <!-- Hamburger Icon -->
       <button @click="isOpen = !isOpen" class="text-white focus:outline-none">
         <Bars3Icon v-if="!isOpen" class="w-8 h-8" />
         <XMarkIcon v-if="isOpen" class="w-8 h-8" />
       </button>
     </div>
 
-    <!-- Mobile Slide-out Menu -->
+    <!-- Slide-out Mobile Menu -->
     <div
       class="fixed top-0 right-0 h-[85vh] w-56 bg-gray-800/90 backdrop-blur-lg shadow-lg rounded-lg transition-transform duration-300"
       :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
@@ -41,35 +41,41 @@ const isHovered = ref(false);
       <ul class="mt-16 space-y-4 text-lg text-white text-center">
         <li class="relative group">
           <RouterLink to="/Login" class="block py-2">Login</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
         <li class="relative group">
           <RouterLink to="/Dashboard" class="block py-2">Dashboard</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
         <li class="relative group">
           <RouterLink to="/Posts" class="block py-2">Posts</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
         <li class="relative group">
           <RouterLink to="/Post" class="block py-2">Post</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
         <li class="relative group">
           <RouterLink to="/Contacts" class="block py-2">Contacts</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
         <li class="relative group">
           <RouterLink to="/Settings" class="block py-2">Settings</RouterLink>
-          <span class="hover-line"></span>
+          <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          ></span>
         </li>
       </ul>
     </div>
   </nav>
 </template>
-
-<style scoped>
-.hover-line {
-  @apply absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-red-500 opacity-0 transition-opacity duration-300 group-hover:opacity-100;
-}
-</style>
